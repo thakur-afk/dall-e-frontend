2a9a43db50565f805924be2ab9b3219fc9af7953
Harshit Thakur <harshitthakur@ternaengg.ac.in>
2023-06-08 13:43:42 +0530

    pushing frontend sepratelyin git

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+
+
+// if tailwind npt working -> npx tailwindcss -i ./src/input.css -o ./dist/output.css --watch 
